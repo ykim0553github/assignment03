30acd267734ea7f5489968780035db6238d61e7a
Yongjin Kim <yongjinkim580@gmail.com>
2022-03-18 13:46:35 -0400

    Version 3 change made

---
+++ components/version3change.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function version3change() {
+  return (
+    <View>
+      <Text>version3change</Text>
+    </View>
+  )
+}
